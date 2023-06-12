0b0fdfda5469705f4ca549f6c855c82a5d16fca8
ncip48 <mbahcip00@gmail.com>
2023-06-12 11:29:50 +0700

    up dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\dosen;
+use App\Models\fakultas;
+use App\Models\Prodi;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,9 +13,9 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $jumlah_dosen = 10;
-        $jumlah_fakultas = 5;
-        $jumlah_prodi = 2;
+        $jumlah_dosen = dosen::count();
+        $jumlah_fakultas = fakultas::count();
+        $jumlah_prodi = Prodi::count();
         $jumlah_user = User::count();
         $jumlah_matkul = 1;
         $jumlah_rps = 2;
